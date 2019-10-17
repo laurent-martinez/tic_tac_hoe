dc9a740716b63799dcde71ea5530e0b082f57d15
mehdi <mourzagh.m@live.fr>
2019-10-17 12:57:17 +0200

    version 1.0 board

---
+++ lib/board.rb
@@ -0,0 +1,30 @@
+class Board
+attr_accessor :alphabet_array, :numerique_array, :board_hash, :board_array
+  def initialize
+
+    @alphabet_array = ["a", "b", "c"]
+    @numerique_array = [1, 2, 3]
+    @board_array = Array.new(){Array.new()}
+  end
+
+  def create_board
+
+    for letter in 0..2
+      for number in 0..2
+         @board_array = @alphabet_array[letter], @numerique_array[number]
+         print @board_array
+      end
+      puts "\n"
+    end
+  end
+
+#   def display_board
+#     @board_hash.each do |key, value|
+#
+#       print "[#{key}#{value}]"
+#       end
+#     end
+#   end
+end
+board = Board.new()
+board.create_board
